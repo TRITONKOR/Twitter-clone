a6f919dfc2d9374865cb65a922a0bfa949774ec3
TRITONKOR <sasalahotskij@gmail.com>
2026-04-30 00:18:18 +0300

    feat: user auth

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+    before_action :require_login
+
+    def index
+        @posts = Post.where(user_id: current_user_and_following_ids)
+                     .order(created_at: :desc)
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = current_user.posts.build(post_params)
+
+        if @post.save
+            redirect_to posts_path, notice: "Post created successfully."
+        else
+            flash.now[:alert] = "Failed to create post."
+            render :new
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:content)
+    end
+
+    def current_user_and_following_ids
+        following_ids = Relationship.where(follower_id: current_user.id).pluck(:followed_id)
+        following_ids << current_user.id
+        following_ids
+    end
 end
